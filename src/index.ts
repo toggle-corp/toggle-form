68f3d33f336dc4da3a79bffb98b5f5951a207cd6
tnagorra <weathermist@gmail.com>
2021-04-09 16:24:22 +0545

    Export ArraySchema

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
-export { createSubmitHandler, useFormObject, useFormArray, default as useForm } from './form';
-export type { EntriesAsList, EntriesAsKeyValue } from './form';
-export { removeNull, accumulateValues, accumulateErrors, accumulateDifferentialErrors, analyzeErrors } from './schema';
-export type { Schema, LiteralSchema, ObjectSchema, Error, LeafError, ArrayError, ObjectError } from './schema';
-export type { PartialForm, PurgeNull } from './types';
-export { isLocalUrl, isValidUrl } from './utils';
+export {
+    createSubmitHandler,
+
+    useFormObject,
+    useFormArray,
+    default as useForm,
+} from './form';
+export {
+    removeNull,
+
+    accumulateValues,
+    accumulateErrors,
+    accumulateDifferentialErrors,
+    analyzeErrors,
+} from './schema';
+export {
+    isLocalUrl,
+    isValidUrl,
+} from './utils';
 export * from './validation';
+
+export type {
+    Schema,
+    LiteralSchema,
+    ArraySchema,
+    ObjectSchema,
+    Error,
+    LeafError,
+    ArrayError,
+    ObjectError,
+} from './schema';
+export type {
+    PartialForm,
+    PurgeNull,
+} from './types';
+export type {
+    EntriesAsList,
+    EntriesAsKeyValue,
+} from './form';
