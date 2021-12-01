1ce8558fcec16dfe930c7ee164284e77fc524b0a
tnagorra <weathermist@gmail.com>
2021-12-01 09:54:52 +0545

    Update doNotReset flag to make setValue behave similarly to setFieldValue
    - removeNull to ignore certain keys

--- src/nullHelper.js
+++ src/nullHelper.js
@@ -5,7 +5,7 @@ import {
 } from '@togglecorp/fujs';
 
 // eslint-disable-next-line import/prefer-default-export
-export function removeNull(data) {
+export function removeNull(data, ignoreKeys = ['__typename']) {
     if (data === null || data === undefined) {
         return undefined;
     }
@@ -16,6 +16,10 @@ export function removeNull(data) {
         let newData = {};
         Object.keys(data).forEach((k) => {
             const key = k;
+            if (ignoreKeys && ignoreKeys.includes(key)) {
+                return;
+            }
+
             const val = data[key];
             const newEntry = removeNull(val);
             if (isDefined(newEntry)) {
