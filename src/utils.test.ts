71bb0445b8cc96c3d4ca94298b5f11884c29e32f
Priyesh777 <priyeshgautam1010@gmail.com>
2022-03-18 11:35:34 +0545

    Add unit test cases for error-access-helper

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,6 +1,7 @@
 import { isDefined } from '@togglecorp/fujs';
 import {
     hasNoKeys,
+    hasNoValues,
     isDefinedString,
     isLocalUrl,
     isValidUrl,
@@ -11,7 +12,7 @@ test('test isLocalUrl condition', () => {
     expect(isLocalUrl('https://www.localhost.com')).toBe(false);
     expect(isLocalUrl('localhost.com')).toBe(false);
     expect(isLocalUrl('')).toBe(false);
-    expect(isLocalUrl('random-string')).toBe(false);
+    expect(isLocalUrl('localhost')).toBe(false);
 });
 
 test('test isValidUrl condition', () => {
@@ -26,7 +27,7 @@ test('test isValidUrl condition', () => {
 test('test isDefined condition', () => {
     expect(isDefined(null)).toBe(false);
     expect(isDefined(undefined)).toBe(false);
-    expect(isDefined('')).toBe(false);
+    expect(isDefined('')).toBe(true);
     expect(isDefined('Hello jest')).toBe(true);
     expect(isDefined(40)).toBe(true);
     expect(isDefined(-40)).toBe(true);
@@ -37,14 +38,42 @@ test('test isDefinedString condition', () => {
     expect(isDefinedString(null)).toBe(false);
     expect(isDefinedString(undefined)).toBe(false);
     expect(isDefinedString('')).toBe(false);
-    expect(isDefinedString('Hello jest')).toBe(true);
+    expect(isDefinedString('Hello jest ###')).toBe(true);
 });
 
 test('test hasNoKeys condition', () => {
     expect(hasNoKeys(null)).toBe(true);
     expect(hasNoKeys(undefined)).toBe(true);
+    expect(hasNoKeys({})).toBe(true);
     expect(hasNoKeys({
-        a: 'some string',
-        b: 24,
+        1: 'one',
+        2: 'two',
     })).toBe(false);
+    expect(hasNoKeys({
+        1: '',
+        2: [],
+        3: {},
+    })).toBe(false);
+    expect(hasNoKeys({
+        1: -1.2,
+        2: 24,
+        3: {},
+        4: null,
+        5: undefined,
+    })).toBe(false);
+});
+
+test('test hasNoValues condition', () => {
+    expect(hasNoValues(null)).toBe(true);
+    expect(hasNoValues(undefined)).toBe(true);
+    expect(hasNoValues([])).toBe(true);
+    expect(hasNoValues(['', 'word', null, undefined])).toBe(false);
+    expect(hasNoValues([
+        '',
+        [],
+        {},
+        -1.2,
+        24,
+        'name',
+    ])).toBe(false);
 });
